2df21ab575032adf859c468df71e988401f85b45
Edward Brash <brash@jlab.org>
2021-07-26 19:28:36 -0400

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: brash
+ *
+ * Created on July 26, 2021, 7:21 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World!\n");
+    return (EXIT_SUCCESS);
+}
+
