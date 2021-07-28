7c10c6dc373c8c5dc95d3d154409af5503b4afb6
shirl <shirl@DESKTOP-BAOQ9KH>
2021-07-28 11:51:41 -0400

    Updates

--- ECalcPlot/main.c
+++ ECalcPlot/main.c
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Function to calculate x^N */
 double power(double x, int N) {
     double result=1.0;
     for (int i=0; i<N; i++){
@@ -21,6 +22,7 @@ double power(double x, int N) {
     return result;
 }
 
+/* Function to approximate e as e = (1+1/N)^N for large N */
 double elim(int N) {
     double x = 1.0+1.0/N;
     return power(x,N);
@@ -28,18 +30,31 @@ double elim(int N) {
 
 int main(int argc, char** argv) {
 
+    /* define the maximum number of iteration values as a constant integer */
     const int N_max = 1200;
     
-    double N[N_max];
+    /* define the value of e to 9 decimal places */
     double e_accurate = 2.718281828;
+    
+    /* create arrays to hold the number of iterations, the value of e for this
+     iteration, the differences between the exact value and our calculation, and
+     our prediction for the difference based on diff = A/N_iterations */
+    double N[N_max];
     double e[N_max], diff[N_max], diff_theory[N_max];
     
+    /* create variables for the initial calculation, which defines the
+     constant A, for our prediction */
     double x0,y0,A;
     
+    /* Loop over the iterations, from 0 to the maximum number*/
     for (int i=0; i<N_max; i++) {
+        
         N[i] = i*15000+15000;
         e[i] = elim(N[i]);
         diff[i] = e_accurate - e[i];
+        
+        /* record the initial value at the lowest number of iterations, which
+         will define the constant A for future predictions */
         if (i==0) {
             x0 = N[i];
             y0 = diff[i];
@@ -53,17 +68,32 @@ int main(int argc, char** argv) {
     
     printf ("Initial x,y value = %9.8f,%9.8f \n",x0,y0);
     
+    
+    /* now that we have the data, create/open a GNUPlot script and data file for later plotting */
+    
     FILE *gnuplot = fopen("gnuplotScript", "w");
     FILE *gnudata = fopen("gnuplotData", "w");
+    
+    /* Mac OS terminal type */
     fprintf(gnuplot, "set terminal x11\n");
+    /* Windows 10 terminal type */
+    /* fprintf(gnuplot, "set terminal qt\n"); */
+    
+    /* Set logscale, title, and axis labels */
     fprintf(gnuplot, "set logscale xy\n");
     fprintf(gnuplot, "set title 'Calculation of e'\n");
     fprintf(gnuplot, "set xlabel 'Number of iterations'\n");
     fprintf(gnuplot, "set ylabel 'Difference from exact value'\n");
+    
+    /* plot command - plot the data as points, and the prediction as a solid line (green)*/
     fprintf(gnuplot, "plot 'gnuplotData' using 1:2, 'gnuplotData' using 1:3 with lines linestyle 2\n");
+    
+    /* write the data to be plotted to a file*/
     for (int i = 0; i < N_max; i++) {
         fprintf(gnudata, "%g %g %g\n", N[i], diff[i], diff_theory[i]);
     }
+    
+    /* cleanup */
     fflush(gnuplot);
     fflush(gnudata);
     
