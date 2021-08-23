b789fcf208dfecde4d16b899f1056144f168da28
Edward Brash <brash@jlab.org>
2021-08-22 23:52:28 -0400

    Updates

---
+++ AverageDistance/nbproject/Makefile-variables.mk
@@ -0,0 +1,43 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=averagedistance
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/averagedistance
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=averagedistance.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/averagedistance.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=averagedistance
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/averagedistance
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=averagedistance.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/averagedistance.tar
+# Performance_Release configuration
+CND_PLATFORM_Performance_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Performance_Release=dist/Performance_Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Performance_Release=averagedistance
+CND_ARTIFACT_PATH_Performance_Release=dist/Performance_Release/GNU-MacOSX/averagedistance
+CND_PACKAGE_DIR_Performance_Release=dist/Performance_Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Performance_Release=averagedistance.tar
+CND_PACKAGE_PATH_Performance_Release=dist/Performance_Release/GNU-MacOSX/package/averagedistance.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
