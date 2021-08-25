e6b98d1a86a78d6d09693536dab930156543a65a
Edward Brash <brash@jlab.org>
2021-08-25 15:21:12 -0400

    Update

---
+++ FileStatistics/main.c
@@ -0,0 +1,74 @@
+/*
+ * CPSC 256 - C/C+ for Scientists and Engineers
+ * Christopher Newport University
+ * Department of Physics, Computer Science & Engineering
+ */
+
+/* 
+ * File:   main.c
+ * Author: brash
+ *
+ * Created on August 25, 2021, 2:09 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <gplot.h>
+
+/*
+ * 
+ */
+
+double mean(double [], int);
+double stdev(double[], int);
+
+int main(int argc, char** argv) {
+    
+    const int N_Max = 10000;
+    double inputData[N_Max];
+    FILE *inputFilePointer;
+    
+    inputFilePointer = fopen("inputData.dat","r");
+    
+    int i = 0;
+    if (inputFilePointer != NULL) 
+        do
+        {
+            fscanf(inputFilePointer,"%lf",&inputData[i]);
+            i++;
+        } while (!feof(inputFilePointer));
+    
+    printf("Number of elements read from file = %d\n",(i-1));
+    const int N_Elem = i-1;
+    
+    printf("Mean value of elements = %g\n",mean(inputData,N_Elem));
+    printf("Std. Dev. of elements = %g\n",stdev(inputData,N_Elem));
+    
+    return (EXIT_SUCCESS);
+}
+
+double mean(double a[], int n) {
+    double sum = 0.0;
+    for (int i=0; i<n; i++) {
+        sum = sum + a[i];
+    }
+    double mean = sum/n;
+    
+    return mean;
+}
+
+double stdev(double a[], int n) {
+    
+    double localmean = mean(a,n);
+    double sum2 = 0.0;
+    
+    for (int i=0;i<n;i++) {
+        sum2 = sum2 + pow((a[i]-localmean),2);
+    }
+    
+    sum2 = sum2 / (n-1);
+    double stdev = sqrt(sum2);
+    
+    return stdev;
+}
