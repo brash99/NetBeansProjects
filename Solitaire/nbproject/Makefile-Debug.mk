92c80b2208bef13efd3af9a0e99721587e8a112b
Edward Brash <brash@jlab.org>
2021-08-28 00:38:49 -0400

    updates

--- Solitaire/nbproject/Makefile-Debug.mk
+++ Solitaire/nbproject/Makefile-Debug.mk
@@ -21,8 +21,8 @@ FC=gfortran
 AS=as
 
 # Macros
-CND_PLATFORM=MinGW-Windows
-CND_DLIB_EXT=dll
+CND_PLATFORM=GNU-MacOSX
+CND_DLIB_EXT=dylib
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
@@ -52,13 +52,13 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=-L../gplot/dist/Debug/MinGW-Windows -lgplot -lm
+LDLIBSOPTIONS=-L../gplot/dist/Debug/MinGW-Windows -L../gplot/dist/Debug/GNU-MacOSX -lgplot -lm
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/solitaire.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/solitaire
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/solitaire.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/solitaire: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/solitaire ${OBJECTFILES} ${LDLIBSOPTIONS}
 
