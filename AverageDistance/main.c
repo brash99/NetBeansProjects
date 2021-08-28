768dbff53e6d243821fef17bcaf833865402f9f6
shirl <shirl@DESKTOP-BAOQ9KH>
2021-08-27 22:04:03 -0400

    Updates

--- AverageDistance/main.c
+++ AverageDistance/main.c
@@ -9,13 +9,15 @@
  * Author: brash
  *
  * Created on August 22, 2021, 4:15 PM
+ * 
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <gplot.h>
-#include <sodium.h>
+#include <time.h>
+#include <mtwister.h>
 
 /*
  * 
@@ -23,15 +25,16 @@
 int main(int argc, char** argv) {
        
     double sum = 0.0;
-    const int N_max = 5000000;
+    const int N_max = 50000;
+    MTRand r = seedRand((unsigned)time(0));
     
     double distance[N_max];
     
     for (int i=0; i<N_max; i++) {
-        double x1 = 1.0*randombytes_uniform(RAND_MAX)/RAND_MAX;
-        double x2 = 1.0*randombytes_uniform(RAND_MAX)/RAND_MAX;
-        double y1 = 1.0*randombytes_uniform(RAND_MAX)/RAND_MAX;
-        double y2 = 1.0*randombytes_uniform(RAND_MAX)/RAND_MAX;
+        double x1 = genRand(&r);
+        double x2 = genRand(&r);
+        double y1 = genRand(&r);
+        double y2 = genRand(&r);
         double dist = sqrt(pow((x1-x2),2)+pow((y1-y2),2));
         distance[i]=dist*100.0;
         /* printf("%d         %g\n",i,dist); */
@@ -40,7 +43,7 @@ int main(int argc, char** argv) {
     
     int binwidth = 1;
     int binstart = 0;
-    /* gplot_hist_basic(distance,N_max,binwidth,binstart,"Average Distance Simulation","Distance x 100","Simulated Data","Unix"); */
+    gplot_hist_basic(distance,N_max,binwidth,binstart,"Average Distance Simulation","Distance x 100","Simulated Data","Windows"); 
     
     double avg = sum/N_max;
     printf("Average =          %g\n",avg);
