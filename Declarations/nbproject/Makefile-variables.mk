4822b9d09e453e706114d5089c5d96b723b17507
Edward Brash <brash@jlab.org>
2021-09-02 11:51:45 -0400

    Updates

--- Declarations/nbproject/Makefile-variables.mk
+++ Declarations/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-MacOSX
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=declarations
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/declarations
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/declarations
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=declarations.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/declarations.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/declarations.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-MacOSX
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
 CND_ARTIFACT_NAME_Release=declarations
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/declarations
-CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/declarations
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
 CND_PACKAGE_NAME_Release=declarations.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/declarations.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/declarations.tar
 #
 # include compiler specific variables
 #
