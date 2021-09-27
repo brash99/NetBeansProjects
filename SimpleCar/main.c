afb2996c20665442a3b4979ab1ac0183214ee4fa
Edward Brash <brash@jlab.org>
2021-09-27 13:20:20 -0400

    update

--- SimpleCar/main.c
+++ SimpleCar/main.c
@@ -23,9 +23,16 @@ int main() {
    scanf("%d", &milesReverse);
    
    SimpleCar myCar = InitCar();
+   printf("In main 1:  address of myCar = %p\n",&myCar);
    
    myCar = Drive(milesForward,myCar);
+   
+   printf("In main 2:  address of myCar = %p\n",&myCar);
+   
    myCar = Reverse(milesReverse,myCar);
+   
+   printf("In main 3:  address of myCar = %p\n",&myCar);
+   
    HonkHorn(myCar);   
    Report(myCar);
   
