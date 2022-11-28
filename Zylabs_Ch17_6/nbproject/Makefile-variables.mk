c99a6c74ef8a687c976408ad71bbbf374099ef58
brash99 <brash@jlab.org>
2022-11-27 20:07:53 -0500

    Updates

---
+++ Zylabs_Ch17_6/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=zylabs_ch17_6
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/zylabs_ch17_6
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=zylabsch176.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/zylabsch176.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=zylabs_ch17_6
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/zylabs_ch17_6
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=zylabsch176.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/zylabsch176.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
